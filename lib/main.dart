d895f1eca3e4924c5c06fccf75b041a4b9932054
Diego Belay <diego.belay@gmail.com>
2021-07-19 23:53:55 -0300

    feat: named routes

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,29 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        ChatScreen.id: (context) => ChatScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+      },
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute<void>(
+          settings: settings,
+          builder: (BuildContext context) => Scaffold(
+            backgroundColor: Colors.green,
+            body: Center(
+                child: Text(
+              'Cómo llegaste acá ?\n:V',
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
+          ),
+        );
+      },
     );
   }
 }
