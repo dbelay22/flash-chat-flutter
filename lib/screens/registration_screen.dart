9aee867534c321f856aaa651b1ceb82996d96fe3
Diego Belay <diego.belay@gmail.com>
2021-08-05 10:23:31 -0300

    feat: add firebase auth email registration

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/components/rounded_button.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 
@@ -9,6 +11,10 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,8 +35,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardType: TextInputType.emailAddress,
+              textAlign: TextAlign.center,
               onChanged: (value) {
-                //Do something with the user input.
+                email = value;
               },
               decoration: kTextFieldDecoration.copyWith(
                 hintText: 'Ingrese su dirección de correo electrónico',
@@ -40,8 +48,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 8.0,
             ),
             TextField(
+              textAlign: TextAlign.center,
+              obscureText: true,
               onChanged: (value) {
-                //Do something with the user input.
+                password = value;
               },
               decoration: kTextFieldDecoration.copyWith(
                 hintText: 'Ingrese su contraseña',
@@ -52,12 +62,27 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             ),
             RoundedButton(
               label: 'Register',
-              onPressed: () {},
+              onPressed: () async {
+                print('$email - $password');
+                await createUser(context);
+              },
               color: Colors.blueAccent,
             ),
           ],
         ),
       ),
     );
+  }
+
+  Future createUser(BuildContext context) async {
+    try {
+      final newUser = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      if (newUser != null) {
+        Navigator.pushNamed(context, ChatScreen.id);
+      }
+    } catch (e) {
+      print('error al crear usuario $e');
+    }
   }
 }
