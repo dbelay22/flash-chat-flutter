1b7c541c7ea398e2fd487498a4c8e8c64804a9ec
Diego Belay <diego.belay@gmail.com>
2021-08-22 23:29:56 -0300

    feat: add progress modal HUD

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flash_chat/components/rounded_button.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'chat_screen.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -16,8 +16,9 @@ class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
   String email;
   String password;
+  bool loginInProgress = false;
 
-  void login(BuildContext context) async {
+  Future login(BuildContext context) async {
     // pwd: asdasd
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
@@ -27,63 +28,74 @@ class _LoginScreenState extends State<LoginScreen> {
       }
     } catch (e) {
       print('error al crear usuario $e');
+    } finally {
+      loginInProgress = false;
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 222.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: loginInProgress,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 222.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                setState(() {
-                  email = value;
-                });
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Ingrese su dirección de correo electrónico',
+              SizedBox(
+                height: 48.0,
               ),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Ingrese su contraseña',
+              TextField(
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Ingrese su dirección de correo electrónico',
+                ),
               ),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              label: 'Login',
-              onPressed: () {
-                login(context);
-              },
-              color: Colors.lightBlueAccent,
-            ),
-          ],
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Ingrese su contraseña',
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                label: 'Login',
+                onPressed: () async {
+                  setState(() {
+                    loginInProgress = true;
+                  });
+                  await login(context);
+                  setState(() {
+                    loginInProgress = false;
+                  });
+                },
+                color: Colors.lightBlueAccent,
+              ),
+            ],
+          ),
         ),
       ),
     );
