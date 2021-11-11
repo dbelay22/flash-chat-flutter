a7ee667fcef55c2b4976cdff6083b42bc73b5151
Diego Belay <diego.belay@gmail.com>
2021-11-11 10:08:04 -0300

    chore: migrate to flutter embedding v2

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/diego.belay/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/diego.belay/dev/flutterProjects/flash-chat-flutter"
+export "FLUTTER_ROOT=/Volumes/H2D2/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/dlmb/dev/flutterProjects/bootcampProjects/flash-chat-flutter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
